9994bd78203db5fbf9fae5322e943f8e16cde121
Aravind Krishnan <aravind.krishnan@partner.bmw.de>
2019-04-24 13:20:39 +0200

    initial commit

---
+++ example.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+namespace ct
+{
+
+}
+int main(int argc, char* argv[])
+{   // example program
+    using namespace ct;
+
+    int i1 = 3.5;
+    int i2(3.6);
+
+    //int i3{3.7};
+    //int i4= {3.8};
+
+
+    return 0;
+}
